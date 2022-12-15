6f6e21da960cd212bae5a73dfc94d6541dcba630
Local_PC <sv7230@srmist.edu.in>
2022-12-15 12:43:31 +0530

    Project Successfull Done

--- README.md
+++ README.md
@@ -3,6 +3,9 @@ Instagram Scraper using SOAX Residential Proxies.
 Soax is used to prevent our account to get unnoticed.
 Install Chrome web driver
 setup virtual environment
+login instagram by targeting usename and password and login button
+
+target search box and clear it. and enter details into it. 
 
 
 
--- scrapper.py
+++ scrapper.py
@@ -23,8 +23,55 @@ driver = webdriver.Chrome(service = Service(PATH),options=options)
 driver.get("https://www.instagram.com/login")
 
 #target usename section to add username to the username section
-username = WebDriverWait(driver, 10).until(EC.element_to_beclickable((By.CSS_SELECTOR, "input=[name=username]")))
-password = WebDriverWait(driver, 10).until(EC.element_to_beclickable((By.CSS_SELECTOR, "input=[name=password]")))
+username = WebDriverWait(driver, 10).until(EC.element_to_beclickable((By.CSS_SELECTOR, "input=[name=username")))
+password = WebDriverWait(driver, 10).until(EC.element_to_beclickable((By.CSS_SELECTOR, "input=[name=password")))
+
+#enter the details of username and password
+    #Send keys to pass the arguments. 
+username.clear()
+username.send_keys("Enter your userName")
+password.clear()
+password.send_keys("Enter your password")
+
+#hit login button
+
+button = WebDriverWait(driver, 10).until(EC.element_to_beclickable((By.CSS_SELECTOR, "input=[type='submit']")))
+button.click()
+
+#target seach box and ennter input into it.
+searchbox = WebDriverWait(driver, 10).until(EC.element_to_beclickable((By.XPATH, "//input=[@placeholder='Search']")))
+searchbox.clear()
+
+keyword = "#bikes"     #you can enter your desirable keyword
+searchbox.send_keys(keyword)
+
+#wait for 5 seconds
+time.sleep(5)
+searchbox.send_keys(keys.ENTER)  #this is press enter on keyboard
+
+#have to press enter two times to land to a search page. 
+time.sleep(5)
+searchbox.send_keys(keys.ENTER)
+
+
+#scroll down for more images
+driver.execute.script("window.scrollTo(0,4000);")
+
+#taget image
+time.sleep(5)
+images = driver.find_elements(By.TAG_NAME, 'img')
+images = [images.get_attribute('src') for image in images]
+images = images[:-2]
+
+#no of images linked
+print("Totol number of scrapped images: ", len(images))
+
+#display targeted image 
+print("Displaying Images: ", images[4])
+driver.get(images[4])
+
+
+
 
 
 
