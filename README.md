968722f817527496e26c74eaf1847cbe479018ef
Local_PC <sv7230@srmist.edu.in>
2022-12-15 12:19:30 +0530

    Enviroment Set

--- README.md
+++ README.md
@@ -2,5 +2,7 @@
 Instagram Scraper using SOAX Residential Proxies.
 Soax is used to prevent our account to get unnoticed.
 Install Chrome web driver
+setup virtual environment
+
 
 
--- scrapper.py
+++ scrapper.py
@@ -1,6 +1,7 @@
 #import some necessory files, using time and selenium
 import time
 import webbrowser
+import selenium
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
@@ -13,9 +14,18 @@ from selenium.webdriver.support.wait import WebDriverWait
 PROXY =  "proxy.soax.com:10002"
 
 PATH = "./chromedriver"
-options = webbrowser.ChromeOptions()
+options = webdriver.ChromeOptions()
 options.add_argument('proxy.soax.com'.format(PROXY))
 driver = webdriver.Chrome(service = Service(PATH),options=options)
+
+
+#Open Instagram and Login in it. 
+driver.get("https://www.instagram.com/login")
+
+#target usename section to add username to the username section
+username = WebDriverWait(driver, 10).until(EC.element_to_beclickable((By.CSS_SELECTOR, "input=[name=username]")))
+password = WebDriverWait(driver, 10).until(EC.element_to_beclickable((By.CSS_SELECTOR, "input=[name=password]")))
+
 
 
 
